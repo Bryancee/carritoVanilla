fed9e089a847cd7f8ae94571dede07dede927c8d
Bryancee <612110528@utsalamanca.edu.mx>
2022-10-04 14:58:55 -0500

    Agregamos botons y todo

--- js/app.js
+++ js/app.js
@@ -17,6 +17,90 @@ document.addEventListener('DOMContentLoaded', e =>{ /*e ó () son lo mismo*/
   obtenerProductos()
 })
 
+cards.addEventListener('click', (e) => {
+	//console.log(e)
+	addCarrito(e)
+})
+
+const addCarrito = (e) => {
+	if(e.target.classList.contains('btn-dark')){
+		//alert('boton')
+		setCarrito(e.target.parentElement)
+	}
+	e.stopPropagation()
+}
+
+const setCarrito = (item) => {
+	//console.log(item)
+	const producto = {
+		title: item.querySelector('h5').textContent,
+		precio: item.querySelector('p').textContent,
+		id: item.querySelector('button').dataset.id,
+		cantidad: 1
+	}
+		//console.log(producto)
+
+		if(carrito.hasOwnProperty(producto.id)){//VERIFICA EN EL ARREGLO SI EXISTE LA POSICION
+			producto.cantidad = carrito[producto.id].cantidad + 1
+		}
+		carrito[producto.id] = {...producto} //...nom_OBJ se sustituyen / empujar
+		pintarCarrito()
+		//console.log(carrito)
+}
+
+const pintarCarrito = () => {
+	items.innerHTML = ''
+	Object.values(carrito).forEach(item => {
+
+		//AGREGAR INFORMACION A LA TABLA AL CLICKEAR
+		templateCarrito.querySelector('th').textContent = item.id
+		templateCarrito.querySelectorAll('td')[0].textContent = item.title
+		templateCarrito.querySelectorAll('td')[1].textContent = item.cantidad
+		templateCarrito.querySelector('span').textContent = item.precio * item.cantidad
+
+		//BOTONES
+		templateCarrito.querySelector('.btn-info').dataset.id = item.id
+		templateCarrito.querySelector('.btn-danger').dataset.id = item.id
+
+		const clone = templateCarrito.cloneNode(true)
+		fragment.appendChild(clone)
+	})
+	items.appendChild(fragment)
+
+	pintarFooter()
+}
+
+const pintarFooter = () => {
+	footer.innerHTML = ''
+	if(Object.keys(carrito).length === 0){
+		footer.innerHTML = 
+	`
+	<th colspan="5" scope="row">Carrito vacío - ¡Compra!</th>
+	`		
+	return
+	}
+
+	// Calcular Totales
+	const nCantidad = Object.values(carrito).reduce((
+		acc, { cantidad}
+	) => acc + cantidad, 0)
+	const nPrecio = Object.values(carrito).reduce((
+		acc, { cantidad, precio}
+	) => acc + (cantidad * precio), 0)
+	templateFooter.querySelectorAll('td')[0].textContent = nCantidad
+	templateFooter.querySelector('span').textContent = nPrecio
+
+	const clone = templateFooter.cloneNode(true)
+	fragment.appendChild(clone)
+	footer.appendChild(fragment)
+
+	const boton = document.querySelector('#vaciar-carrito')
+	boton.addEventListener('click', () => {
+		carrito = {}
+		pintarCarrito()
+	})
+}
+
 const obtenerProductos = async() => {
   const res = await fetch('./api/productos.json') /*LEER LO QUE ESTÁ DENTRO DEL ARCHIVO */
 	const data = await res.json()
@@ -28,11 +112,11 @@ const pintarCards = (data) => {
 	data.forEach(item => {
 		templateCard.querySelector('h5').textContent = item.title 
 		templateCard.querySelector('p').textContent = item.precio 
+		templateCard.querySelector('img').setAttribute('src',item.url)
 		templateCard.querySelector('button').dataset.id = item.id 
 
 		const clone = templateCard.cloneNode(true)
 		fragment.appendChild(clone)
 	})
 	cards.appendChild(fragment)
-
 }
